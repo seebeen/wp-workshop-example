8b2b30549d4e3685fe75be0cd539faf6d1c8a174
Sibin Grasic <sibin.grasic@oblak.studio>
2021-05-08 12:41:03 +0200

    feat(core): Removed kontakt page

--- functions.php
+++ functions.php
@@ -5,3 +5,13 @@ add_action('after_setup_theme', function() {
     add_image_size('insane-size', 2560, 1440, false);
 
 });
+
+add_action('template_redirect', function() {
+
+    if (is_page('kontakt')) :
+        wp_safe_redirect('/kontakt-2', 301);
+        exit;
+    endif;
+    
+
+});
