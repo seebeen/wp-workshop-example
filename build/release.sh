dbc2508eac9ea55e4201d7cf015ff852d0b98ccb
Sibin Grasic <sibin.grasic@oblak.studio>
2021-05-08 13:24:12 +0200

    feat(build): ACTUALLY added build files

---
+++ build/release.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+NEXT_VERSION=$1
+CURRENT_VERSION=$(cat style.css | grep Version | head -1 | awk -F= "{ print $2 }" | sed 's/[Version:,\",]//g' | tr -d '[[:space:]]')
+
+sed -ie "s/Version:           $CURRENT_VERSION/Version:           $NEXT_VERSION/g" style.css
+rm -rf style.csse
+
+zip -r ./build/electro-child-$NEXT_VERSION.zip ./*.php ./style.css dist config framework vendor woocommerce -q
